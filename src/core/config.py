8569c7110670fae68a2d4d7a5873ee145a680b50
Alex Howard <thezanke@gmail.com>
2025-08-21 00:52:03 -0400

    Initial commit

---
+++ src/core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+    DATABASE_URI: str
+
+
+settings = Settings()
