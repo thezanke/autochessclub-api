6bf92ae002e1b94ba2f0ac17ed5a933703b65187
Alex Howard <thezanke@gmail.com>
2025-08-21 09:00:22 -0400

    Add game routes to the FastAPI application

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, FastAPI
 
-from src.routes import health
+from src.routes import health, game
 
 app = FastAPI(root_path="/api")
 
 router = APIRouter()
 router.include_router(health.router)
+router.include_router(game.router)
 
 app.include_router(router)
